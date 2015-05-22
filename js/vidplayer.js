accf14f0be85cb254534dd6ee4f215c3a453c52c
arebe <rfboyce@gmail.com>
2015-05-21 20:46:17 -0400

    looping title highlighting

--- js/vidplayer.js
+++ js/vidplayer.js
@@ -156,7 +156,13 @@ function onMainPlayerStateChange(event) {
             $("#vaultlist").empty();
             $("#vaultlist").append($("<ul></ul>").attr({"class": "list-unstyled"}));
             for (var i = 0; i < vaultdata[currentModule].length; i++){
-              $("#vaultlist ul").append($("<li>"+vaultdata[currentModule][i].item_title+"</li>").attr("class","item_title"));
+              $("#vaultlist ul").append($("<li>"+vaultdata[currentModule][i].item_title+"</li>")
+                .attr("class","item_title")
+                .click(function(){
+                  console.log("click added to: "+currentModule+", "+i);
+                  displayItem(currentModule, i);
+                })
+              );
             }
             $("#vaultlist ul li:nth-child(1)").addClass("item_current");
 
@@ -175,27 +181,26 @@ function onMainPlayerStateChange(event) {
 
   var clickIndex = 0;
   $("#next-bt").click(function() {
-      clickIndex = clickIndex + 1;;       
-      if(clickIndex >= vaultdata[currentModule].length) clickIndex = 0;
-
-    $("#currentembed").html("");
-    var v = vaultdata[currentModule][clickIndex];
-    $("#currentembed").append($("<a></a>") .attr({"class":"tracks","itemid":v.itemid+'_tracks',"href":v.itemid,"data-file":v.trackFile}).html(v.location));
-      $('#player-digital-title-one').html(v.description);
-      $('#player-digital-name-one').html(v.item_title);
-      var vaultshow=document.getElementById("vault_null");
-      vaultshow.style.display = "none";
-      var infoshow=document.getElementById("infobox");
-      infoshow.style.display = "block";
-      $("#vaultlist ul li:nth-child("+(clickIndex)+")").removeClass("item_current");
-      $("#vaultlist ul li:nth-child("+(clickIndex+1)+")").addClass("item_current");
+    $("#vaultlist ul li:nth-child("+(clickIndex+1)+")").removeClass("item_current");
+    clickIndex = clickIndex + 1;;       
+    if(clickIndex >= vaultdata[currentModule].length) clickIndex = 0;
+    $("#vaultlist ul li:nth-child("+(clickIndex+1)+")").addClass("item_current");
+    displayItem(currentModule, clickIndex);
   });
 
   $("#prev-bt").click(function() {
+      $("#vaultlist ul li:nth-child("+(clickIndex+1)+")").removeClass("item_current");
       clickIndex = clickIndex - 1;;       
-      if(clickIndex <= 0) clickIndex = vaultdata[currentModule].length - 1;
-      $("#currentembed").html("");
-      var v = vaultdata[currentModule][clickIndex];
+      if(clickIndex < 0) clickIndex = vaultdata[currentModule].length - 1;
+      $("#vaultlist ul li:nth-child("+(clickIndex+1)+")").addClass("item_current");
+      displayItem(currentModule, clickIndex);
+  });
+}
+}
+
+function displayItem(module, index){
+  $("#currentembed").html("");
+      var v = vaultdata[module][index];
       $("#currentembed").append($("<a></a>") .attr({"class":"tracks","itemid":v.itemid+'_tracks',"href":v.itemid,"data-file":v.trackFile}).html(v.location));
       $('#player-digital-title-one').html(v.description);
       $('#player-digital-name-one').html(v.item_title);
@@ -203,11 +208,8 @@ function onMainPlayerStateChange(event) {
       vaultshow.style.display = "none";
       var infoshow=document.getElementById("infobox");
       infoshow.style.display = "block";
-      $("#vaultlist ul li:nth-child("+(clickIndex+2)+")").removeClass("item_current");
-      $("#vaultlist ul li:nth-child("+(clickIndex+1)+")").addClass("item_current");
-  });
 }
-}
+
   function stopVideo() {
     main_player.stopVideo();
   }
